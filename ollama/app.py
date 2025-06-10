8f90e3fd133e137c059426cd8fc8462564c7b897
Mayank Sethi <mayanksethi324@gmail.com>
2025-06-10 13:10:58 +0530

    Update: app.py file

--- ollama/app.py
+++ ollama/app.py
@@ -4,10 +4,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 ## Langsmith Tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 
 # import ollama
 from langchain_ollama import OllamaLLM
